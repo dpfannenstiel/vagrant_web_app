a9c514d67f244b3763f2a4ba99b347a25453d0c8
Dustin Pfannenstiel <dustin@nth-estate.com>
2017-04-08 18:40:49 -0500

    working

---
+++ provision/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+# setup nginx
+# nginx
+sudo apt-get -y install nginx
+sudo service nginx start
+
+# set up nginx server
+sudo cp /vagrant/provision/sample.conf /etc/nginx/sites-available/site.conf
+sudo chmod 644 /etc/nginx/sites-available/site.conf
+sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+sudo service nginx restart
+
+# clean /var/www
+sudo rm -Rf /var/www
+
+# symlink /var/www => /vagrant
+ln -s /vagrant/web_app /var/www
